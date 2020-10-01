4d28ee8eb1745e09197cacee1147146c7f83407e
ShravanCool <shravan.akku@gmail.com>
2020-10-01 19:47:15 +0530

    Added rest in steganography

--- steg1.py
+++ steg1.py
@@ -47,6 +47,66 @@ def hideData(image, secret_message):
     
     return image
 
+def showData(image):
+    binary_data = ""
+    for values in image:
+        for pixel in values:
+            r, g, b = messageToBinary(pixel)
+            binary_data += r[-1]
+            binary_data += g[-1]
+            binary_data += b[-1]
+
+    all_bytes = [binary_data[i:i+8] for i in range(0,len(binary_data),8) ]
+    
+    decoded_data = ""
+    for byte in all_bytes:
+        decoded_data += chr(int(byte,2))
+        if decoded_data[-5:] == "#####":
+            break
+
+    return decoded_data[:-5]
+
+def encoded_text():
+    image_name = input("Enter image name (with extension): ")
+    image = cv2.imread(image_name)
+
+    print("The shape of the image is: ",image.shape)
+    print("The original image is: ")
+    resized_image = cv2.resize(image, (500, 500))
+    cv2_imshow(resized_image)
+
+    data = input("Enter data to be encoded: ")
+    if len(data) == 0:
+        raise ValueError("Data is empty")
+
+    filename = input("Enter the name of new encoded image (with extension): ")
+    encoded_image = hideData(image, data)
+    cv2.imwrite(filename, encoded_image)
+
+def decode_text():
+    image_name = input("Enter the image name to be decoded (with extension): ")
+    image = cv2.imread(image_name)
+
+    print("The Steganographed image is as shown below: ")
+    resized_image = cv2.resize(image, (500, 500))
+
+    text = showData(image)
+    return text
+
+def main():
+    a = input("Image steganography \n 1. Encode the data \n 2. Decode the data \n Your input is: ")
+    userinput = int(a)
+    if userinput == 1:
+        print("\nEncoding....")
+        encode_text()
+    elif userinput == 2:
+        print("\nDecoding....")
+        print("Decoded message is " + decode_text())
+    else:
+        raise Exception("Enter correct input")
+
+main()
+
 
 
 
